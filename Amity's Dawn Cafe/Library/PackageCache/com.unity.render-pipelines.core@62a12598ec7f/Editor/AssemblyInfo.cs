f661f3fc873c126ab40d8b2e6b4050cdd63ad278
angelwing888 <kyliecchang@gmail.com>
2025-11-08 17:23:14 -0800

    sprites

---
+++ Amity's Dawn Cafe/Library/PackageCache/com.unity.render-pipelines.core@62a12598ec7f/Editor/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Unity.RenderPipelines.Core.Editor.Shared")]
+[assembly: InternalsVisibleTo("Unity.RenderPipelines.Core.Editor.Tests")]
+[assembly: InternalsVisibleTo("Unity.RenderPipelines.HighDefinition.Editor.Tests")]
+[assembly: InternalsVisibleTo("Unity.RenderPipelines.Universal.Editor.Tests")]
+[assembly: InternalsVisibleTo("Unity.Testing.VisualEffectGraph.EditorTests")]
+[assembly: InternalsVisibleTo("Assembly-CSharp-Editor-testable")]
+
